a796284d3c82c754ebebc22f7004f4821d564629
rafaelitoortilano101896 <rafaelitoortilano101896@gmail.com>
2025-03-27 03:17:06 +0800

    tes rfid post

--- routes/web.php
+++ routes/web.php
@@ -52,3 +52,10 @@ Route::post('/flexi', function(Request $request) {
 
     return redirect()->route('flexi.remaining-time', ['user' => $flexi->uid]);
 })->name('flexi.remaining-time');
+
+Route::post('/external/rfid-scan', function(Request $request) {
+    $uidResult = $request->input('UIDresult');
+
+    $user = \App\Models\User::find(1);
+    $user->addOrUpdateMeta('rfid', $uidResult);
+});
