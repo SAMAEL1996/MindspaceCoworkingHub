c05174dc21b75d6f0d7f7820ff6679f43cd38987
rafaelitoortilano101896 <rafaelitoortilano101896@gmail.com>
2025-03-29 12:10:21 +0800

    send sms updates

--- database/seeders/SendSmsNotif.php
+++ database/seeders/SendSmsNotif.php
@@ -16,16 +16,24 @@ class SendSmsNotif extends Seeder
      */
     public function run(): void
     {
-        return;
-        
         $this->apiKey = config('app.semaphore_key');
 
-        $content = "Good day! Please be informed of our adjusted schedule in observance of the upcoming holiday:\n\n- October 31 (Open until 10pm only)\n- November 1-3 (Closed)\n- November 4 (Open at 7am, Resuming normal operations)\n\nThank you!";
+        // $content = "Good day! Please be informed of our adjusted schedule in observance of the upcoming holiday:\n\n- October 31 (Open until 10pm only)\n- November 1-3 (Closed)\n- November 4 (Open at 7am, Resuming normal operations)\n\nThank you!";
+        $content = "Please be informed that Mindspace will be closed tomorrow, March 30, 2025 from 8am to 7pm for quarterly maintenance and upgrade. \n\nSpace will re-open on Monday, March 31, 2025 at 7am. \n\nThank you for understanding!";
 
         $flexiUsers = \App\Models\FlexiUser::where('status', true)->get();
         foreach($flexiUsers as $flexi) {
             if($flexi->contact_no) {
                 $this->sendSms($flexi->contact_no, $content);
+
+                usleep(800 * 1000);
+            }
+        }
+
+        $monthlyUsers = \App\Models\MonthlyUser::where('is_expired', false)->get();
+        foreach($monthlyUsers as $monthly) {
+            if($monthly->contact_no) {
+                $this->sendSms($monthly->contact_no, $content);
 
                 usleep(800 * 1000);
             }
