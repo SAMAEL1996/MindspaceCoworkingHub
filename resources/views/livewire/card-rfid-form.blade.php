4261737b68bd0425773cf196d012206b9355b0ff
rafaelitoortilano101896 <rafaelitoortilano101896@gmail.com>
2025-04-01 08:09:05 +0800

    no message

--- resources/views/livewire/card-rfid-form.blade.php
+++ resources/views/livewire/card-rfid-form.blade.php
@@ -3,6 +3,7 @@
         {{ $this->form }}
 
         <div wire:poll.1s="getSessionCard">
+            {{ dd(session()->all()) }}
             @if($rfid)
                 <p>Session Card: {{ $rfid }}</p>
             @else
